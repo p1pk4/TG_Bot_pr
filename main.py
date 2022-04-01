03a47f2aa9639cd8c77f5f9ff07ee228cc36c38d
Bugsy <Toxicbugsy@yandex.com>
2022-04-01 11:49:45 +0300

    create bot menu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import telebot
 from telebot import types
 from config import bot
+
 
 # Список для логов
 user_logs = []
@@ -9,24 +9,48 @@ user_logs = []
 # Старт бота с функциями кнопок
 @bot.message_handler(commands=["start"])
 def start(message, res=False):
-    bot.send_message(message.chat.id, 'First message from bot')
-    url_button(message)
+    url_youtube_button(message)
+    buttons_menu(message)
+    url_vk_button(message)
+
+def url_youtube_button(message):
+     '''URL кнопка'''
+     markup = types.InlineKeyboardMarkup()
+     url_button = types.InlineKeyboardButton('YouTube', url='https://www.youtube.com/watch?v=n0Zli_fQeP8')
+     markup.add(url_button)
+     bot.send_message(message.chat.id, 'Привет, {0.first_name}! Нажми на кнопку'.format(message.from_user),
+                      reply_markup=markup)
+
+def url_vk_button(message):
+     '''URL кнопка'''
+     markup = types.InlineKeyboardMarkup()
+     url_button = types.InlineKeyboardButton('VK', url='https://www.vk.com/feed')
+     markup.add(url_button)
+     bot.send_message(message.chat.id, 'Привет, {0.first_name}! Нажми на кнопку'.format(message.from_user),
+                      reply_markup=markup)
+
+def buttons_menu(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.InlineKeyboardButton('💥 Регистрация 💥')
+
+    button2 = types.InlineKeyboardButton('🤖 Группа в ВКонтакте 🤖')
+
+    button3 = types.InlineKeyboardButton('❤ Магазин ❤')
+
+    button4 = types.InlineKeyboardButton('💌 Обратная связь 💌')
+
+    button5 = types.InlineKeyboardButton('ffffffffffff')
+
+    markup.add(button1, button2, button3, button4, button5)
+
+    bot.send_message(message.chat.id, 'Hey, {0.first_name}!'.format(message.from_user), reply_markup=markup)
 
 
-def url_button(message):
-    '''URL кнопка'''
-    markup = types.InlineKeyboardMarkup()
-    url_button = types.InlineKeyboardButton('youtube', url='https://youtube.com')
-    markup.add(url_button)
-    bot.send_message(message.chat.id, 'Привет, {0.first_name}! Нажми на кнопку'.format(message.from_user),
-                     reply_markup=markup)
-
-
-# Эхо(копия) сообщений от пользователя
-@bot.message_handler(content_types=['text'])
-def handle_text(message):
-    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
-    logs(message)
+# # Эхо(копия) сообщений от пользователя
+# @bot.message_handler(content_types=['text'])
+# def handle_text(message):
+#     bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+#     logs(message)
 
 
 # Запись логов в файл
